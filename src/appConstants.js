fe4409c1ca9b713346e4bd9f242f878817b9c48a
belaczek <tomasbelada@hotmail.com>
2018-04-22 22:16:11 +0200

    feat: add GBP as quote currency

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,7 +1,7 @@
 export const APP_NAME = 'hodl.watch'
 
 export const DEFAULT_QUOTE_SYMBOL = 'USD'
-export const QUOTE_SYMBOL_LIST = ['USD', 'EUR']
+export const QUOTE_SYMBOL_LIST = ['USD', 'EUR', 'GBP']
 
 export const DEFAULT_PROXY_URL = 'https://hodlwatch-proxy.herokuapp.com/'
 
