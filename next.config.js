2384bd6bf1a3f96723cf031d579f9fe9d2105ee5
belaczek <tomasbelada@hotmail.com>
2018-06-24 22:18:26 +0200

    🔨 replace boilerplate by next.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withSass = require('@zeit/next-sass')
+const pipe = require('lodash/fp/pipe')
+
+module.exports = pipe(withSass)({
+  sassLoaderOptions: {
+    includePaths: [
+      'node-modules',
+      'src',
+      require('path').resolve(__dirname, 'node_modules')
+    ]
+  },
+  webpack (config, options) {
+    // Further custom configuration here
+    return config
+  }
+})
