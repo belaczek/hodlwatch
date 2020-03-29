0c0eeb0e033a94f64599f54d2bf43ef03b2a18fb
Tomas Belada <tomasbelada@hotmail.com>
2020-03-29 12:57:06 +0200

    improve redux bootstraping

--- src/utils/decorators/withStore.js
+++ src/utils/decorators/withStore.js
@@ -17,25 +17,8 @@ function getOrCreateStore(initialState) {
   return window[__REDUX_STORE__];
 }
 
-export default App => {
+export default (App) => {
   return class Redux extends React.Component {
-    static async getInitialProps(appContext) {
-      const reduxStore = getOrCreateStore();
-
-      // Provide the store to getInitialProps of pages
-      appContext.ctx.reduxStore = reduxStore;
-
-      let appProps = {};
-      if (App.getInitialProps) {
-        appProps = await App.getInitialProps(appContext);
-      }
-
-      return {
-        ...appProps,
-        initialReduxState: reduxStore.getState()
-      };
-    }
-
     constructor(props) {
       super(props);
       this.reduxStore = getOrCreateStore(props.initialReduxState);
