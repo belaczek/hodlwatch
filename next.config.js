146fb79530e385ee0a3b945cd24a55e31c5498dc
Tomas Belada <tomasbelada@hotmail.com>
2021-05-05 20:13:56 +0200

    chore: update libs and reorganize code
    - Move static assets into src/public/static/ as advised by Next.js
    - update deps and update next.config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const pipe = require("lodash/fp/pipe");
-
-module.exports = pipe(withBundleAnalyzer)({
-  webpack(config, options) {
-    // Further custom configuration here
-    return config;
-  },
-  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: "static",
-      reportFilename: "../../bundles/server.html",
-    },
-    browser: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/client.html",
-    },
-  },
-  distDir: "../.next",
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withBundleAnalyzer({});
