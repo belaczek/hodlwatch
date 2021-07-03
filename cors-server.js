e6d59ee729158af272cc10a6dd16584abd4d5218
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2021-07-03 18:51:07 +0000

    Add server

---
+++ cors-server.js
@@ -0,0 +1,19 @@
+/*
+ * from https://www.npmjs.com/package/cors-anywhere#example
+ */
+
+// Listen on a specific host via the HOST environment constiable
+const host = process.env.HOST || "0.0.0.0";
+// Listen on a specific port via the PORT environment constiable
+const port = process.env.PORT || 8080;
+
+const cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
