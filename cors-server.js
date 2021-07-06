641aaba13a2e1c6fc2d30b8cef4b250325a3db82
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2021-07-06 12:20:51 -0500

    Update cors-server.js

--- cors-server.js
+++ cors-server.js
@@ -12,7 +12,6 @@ cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
   })
   .listen(port, host, function () {
     console.log("Running CORS Anywhere on " + host + ":" + port);
